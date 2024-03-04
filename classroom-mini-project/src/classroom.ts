d770be3176fb077907739d86527b2e33c78c33f3
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-03-04 17:14:59 +0200

    classroom logic and types applied

--- classroom-mini-project/src/classroom.ts
+++ classroom-mini-project/src/classroom.ts
@@ -1,24 +1,23 @@
-export function getNumberOfGrades(grades) {
-    
+export function getNumberOfGrades(grades: number[]) {
+  return grades.length;
 }
 
-export function getSumGrades(grades) {
-    
+export function getSumGrades(grades: number[]) {
+  return grades.reduce((acc, curr) => acc + curr, 0);
 }
 
-export function getAverageValue(grades) {
-    
+export function getAverageValue(grades: number[]) {
+  return getSumGrades(grades) / getNumberOfGrades(grades);
 }
 
-export function getPassingGrades(grades) {
-   
+export function getPassingGrades(grades = []) {
+  return grades.filter((grade) => grade >= 70);
 }
 
-
-export function getFailingGrades(grades) {
-    
+export function getFailingGrades(grades = []) {
+  return grades.filter((grade) => grade < 70);
 }
 
-export function getRaisedGrades(grades) {
-    
+export function getRaisedGrades(grades = []) {
+  return grades.map((grade) => grade + 5);
 }
