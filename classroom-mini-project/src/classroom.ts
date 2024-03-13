fc6f6a182f125bcea39f4ef6fa7f49283e737b55
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-03-13 21:11:06 +0200

    resolved basic functions for grades

--- classroom-mini-project/src/classroom.ts
+++ classroom-mini-project/src/classroom.ts
@@ -1,23 +1,25 @@
-export function getNumberOfGrades(grades: number[]) {
+export function getNumberOfGrades(grades: number[]): number {
   return grades.length;
 }
 
-export function getSumGrades(grades: number[]) {
-  return grades.reduce((acc, curr) => acc + curr, 0);
+export function getSumGrades(grades: number[]): number {
+  return grades.reduce((sum, grade) => sum + grade, 0);
 }
 
-export function getAverageValue(grades: number[]) {
-  return getSumGrades(grades) / getNumberOfGrades(grades);
+export function getAverageValue(grades: number[]): number {
+  const sum = getSumGrades(grades);
+  const numberOfGrades = getNumberOfGrades(grades);
+  return sum / numberOfGrades;
 }
 
-export function getPassingGrades(grades = []) {
-  return grades.filter((grade) => grade >= 70);
+export function getPassingGrades(grades: number[]): number[] {
+  return grades.filter((grade) => grade >= 10);
 }
 
-export function getFailingGrades(grades = []) {
-  return grades.filter((grade) => grade < 70);
+export function getFailingGrades(grades: number[]): number[] {
+  return grades.filter((grade) => grade < 10);
 }
 
-export function getRaisedGrades(grades = []) {
-  return grades.map((grade) => grade + 5);
+export function getRaisedGrades(grades: number[]): number[] {
+  return grades.map((grade) => Math.min(grade + 1, 20));
 }
